b42b51094f337390b150ce9e5d5c86dc322d7c97
Tishamahale25 <Tishamahale25@gmail.com>
2025-04-23 14:47:31 +0530

    save

---
+++ cal.java
@@ -0,0 +1,18 @@
+package cal;
+
+public class cal{
+    public static void main(String[] args) {
+       
+        int num1 = 10;
+        int num2 = 5;
+
+        
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        
+        int difference = num1 - num2;
+        System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+    }
+}
+
