b8345b001dce6bb055d970979f18a846456d690f
Tishamahale25 <Tishamahale25@gmail.com>
2025-04-23 14:52:55 +0530

    master save

--- cal.java
+++ cal.java
@@ -5,6 +5,7 @@ public class cal{
        
         int num1 = 10;
         int num2 = 5;
+        
 
         
         int sum = num1 + num2;
@@ -13,6 +14,12 @@ public class cal{
         
         int difference = num1 - num2;
         System.out.println("The difference between " + num1 + " and " + num2 + " is: " + difference);
+
+        int mul = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + mul);
+
+        int div = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + div);
     }
 }
 
